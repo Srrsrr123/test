a9f9e59e2569478964da059ed98c0eb94a7bae3e
Srrsrr123 <1018959407@qq.com>
2021-05-01 23:32:09 +0800

    Update Home.vue

--- vue01/src/components/Student/Home/Home.vue
+++ vue01/src/components/Student/Home/Home.vue
@@ -4,7 +4,10 @@
     <p>我是首页内容，1111111111111</p>
     <el-button type="warning" >警告按钮</el-button>
     <el-button type="danger" >危险按钮</el-button>
-
+    <p>吼吼吼吼吼吼吼吼吼吼吼吼吼吼吼</p>
+    <p>吼吼吼吼吼吼吼吼吼吼吼吼吼吼吼</p>
+    <p>吼吼吼吼吼吼吼吼吼吼吼吼吼吼吼</p>
+    <p>吼吼吼吼吼吼吼吼吼吼吼吼吼吼吼</p>
   </div>
 </template>
 
